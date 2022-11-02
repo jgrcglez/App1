a042587abe18270eee8defba1af6447ade06e041
Cute Developer <cutedigitaldeveloper@gmail.com>
2022-11-02 16:50:14 -0500

    feat:
    First Commit

---
+++ app.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const express = require('express');
+const bp = require('body-parser');
+const app = express();
+const db = require('./queries');
+
+app.use(bp.json());
+app.use(bp.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.status(200).send('Hello, world!').end();
+});
+
+app.get('/users', db.getUsers);
+app.get('/users/:id', db.getUserById);
+app.post('/users', db.createUser);
+app.put('/users/:id', db.updateUser);
+app.delete('/users/:id', db.deleteUser);
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+    console.log('Press Ctrl+C to quit.');
+});
+
+module.exports = app;
